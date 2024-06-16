de4fbaf3d6bdb0dac990632b53e0b34db769b0cd
Connor Richard Davis <st10068305@vcconnect.edu.za>
2024-06-16 23:58:41 +0200

    Fixes

--- examples/client.js
+++ examples/client.js
@@ -1,72 +1,76 @@
 const gradient = require("gradient-string");
 const inquirer = require("inquirer");
 const { LISTENING } = require("../constants");
+const { IrisProtocolClient } = require("../index");
+const figlet = require("figlet");
 
-const Choices = {
-  DOWNLOAD_FILE: "Download File",
-  EXIT: "Exit",
-};
+(async () => {
+  const Choices = {
+    DOWNLOAD_FILE: "Download File",
+    EXIT: "Exit",
+  };
 
-const { publicKey } = await inquirer.prompt({
-  type: "input",
-  name: "publicKey",
-  message: "What is the host's public key?",
-});
-
-const client = new IrisProtocolClient(publicKey);
-
-client.events.subscribe((rawPacket) => {
-  const packetString = rawPacket.toString();
-  const packet = JSON.parse(packetString);
-
-  switch (packet.type) {
-    case LISTENING:
-      client.socket.on("open", () => {
-        (async () => {
-          await app();
-        })();
-      });
-      break;
-    default:
-      break;
-  }
-});
-
-const app = async () => {
-  process.stdout.write("\x1Bc");
-
-  console.log(gradient.pastel.multiline(figlet.textSync("IrisProtocol")));
-
-  const { option } = await inquirer.prompt({
-    type: "list",
-    name: "option",
-    message: "What would you like to do?",
-    choices: Object.values(Choices),
+  const { publicKey } = await inquirer.prompt({
+    type: "input",
+    name: "publicKey",
+    message: "What is the host's public key?",
   });
 
-  switch (option) {
-    case Choices.DOWNLOAD_FILE:
-      const files = await client.getRemoteFiles();
+  const client = new IrisProtocolClient(publicKey);
 
-      if (files.length > 0) {
-        const { fileName } = await inquirer.prompt({
-          type: "list",
-          name: "fileName",
-          message: "Which file do you want to download?",
-          choices: files.map((file) => file.fileName),
+  client.events.subscribe((rawPacket) => {
+    const packetString = rawPacket.toString();
+    const packet = JSON.parse(packetString);
+
+    switch (packet.type) {
+      case LISTENING:
+        client.socket.on("open", () => {
+          (async () => {
+            await app();
+          })();
         });
+        break;
+      default:
+        break;
+    }
+  });
 
-        await client.downloadFile(fileName, console.log);
-      }
+  const app = async () => {
+    process.stdout.write("\x1Bc");
 
-      break;
+    console.log(gradient.pastel.multiline(figlet.textSync("IrisProtocol")));
 
-    case Choices.EXIT:
-      process.exit(1);
+    const { option } = await inquirer.prompt({
+      type: "list",
+      name: "option",
+      message: "What would you like to do?",
+      choices: Object.values(Choices),
+    });
 
-    default:
-      break;
-  }
+    switch (option) {
+      case Choices.DOWNLOAD_FILE:
+        const files = await client.getRemoteFiles();
 
-  await app();
-};
+        if (files.length > 0) {
+          const { fileName } = await inquirer.prompt({
+            type: "list",
+            name: "fileName",
+            message: "Which file do you want to download?",
+            choices: files.map((file) => file.fileName),
+          });
+
+          await client.downloadFile(fileName, console.log);
+        }
+
+        break;
+
+      case Choices.EXIT:
+        process.exit(1);
+
+      default:
+        break;
+    }
+
+    await app();
+  };
+})();
